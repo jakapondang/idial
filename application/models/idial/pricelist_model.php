25c01be1862b6818873b8e52cc475360fc267363
WhisnuSD <whisnu@shopdeca.com>
2013-12-14 10:31:34 +0700

    latest

--- application/models/idial/pricelist_model.php
+++ application/models/idial/pricelist_model.php
@@ -1,5 +1,5 @@
 <?php
-class Reviews_model extends CI_Model {
+class Pricelist_model extends CI_Model {
    
     
     function __construct() {
@@ -7,48 +7,49 @@ class Reviews_model extends CI_Model {
         $this->load->database();
     }
 
-    function getRowPage(){
-        $table ="jp_page";
-
+    function getCategoryName(){
+        $table = "jp_category";
+        $where = array(
+                "status"=>'1'
+        );
         $query_result ="";
-        $query = $this->db->query(
-            "SELECT * FROM ".$table."  WHERE status = '1'  ORDER BY updated ");
-
-        $query_result = $query->num_rows;
-        return $query_result;
-    }
-    function getPage($limit){
-        $table ="jp_page";
-
-        $query_result ="";
-        $query = $this->db->query(
-            "SELECT * FROM ".$table."  WHERE status = '1'  LIMIT ".$limit);
+        $query = $this->db->get_where($table,$where);
         if($query->num_rows()>0){
 
-            foreach( $query->result()AS $row){
-                $query_result[]= $row->pag_id;
+            foreach($query->result()AS $row){
+                $query_result[$row->cat_id] = $row->name;
             }
         }
-
         return $query_result;
     }
 
-    function getPageMeta($id){
-        $table ="jp_pagemeta";
-
+    function getProductCategory(){
+        $table ="jp_product";
         $query_result ="";
         $query = $this->db->query(
-            "SELECT * FROM ".$table."  WHERE pag_id = '".$id."' ");
+            "SELECT cat_id FROM ".$table." WHERE status = '1' GROUP BY cat_id ORDER BY updated");
         if($query->num_rows()>0){
-            $query_result = $query->result();
 
-
-
+            foreach($query->result()AS $row){
+                $query_result[] = $row->cat_id;
+           }
         }
         return $query_result;
     }
 
+    function getProductList(){
+        $query = $this->db->query(
+            "SELECT A.pro_id AS proid,A.cat_id AS catid,A.bra_id AS braid ,A.name AS name, B.gross AS price ,B.stock AS stock FROM jp_product A
+            INNER JOIN jp_productprice B ON A.pro_id = B.pro_id  WHERE A.status = '1' ORDER BY updated");
 
+        $returnValue ="";
+        if ($query->num_rows() > 0)
+        {
+            $returnValue = $query->result();
+
+        }
+        return $returnValue;
+    }
 
    
 }
